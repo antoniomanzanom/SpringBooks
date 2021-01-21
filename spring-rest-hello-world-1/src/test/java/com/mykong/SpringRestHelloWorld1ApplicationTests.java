32463612c41d6af86d7e8c646372e02ae697d14c
antoniomanzanom <56824072+antoniomanzanom@users.noreply.github.com>
2021-01-21 09:26:26 +0100

    Add files via upload

---
+++ spring-rest-hello-world-1/src/test/java/com/mykong/SpringRestHelloWorld1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykong;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestHelloWorld1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
